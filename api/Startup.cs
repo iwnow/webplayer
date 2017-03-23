a19f143bf4f790f8e0fb54d8f8ad7844d37ef62c
Виталий Вострокнутов <iwnow@airmac-ivvnow.local>
2017-03-23 17:08:41 +0300

    add static file pipe

--- api/Startup.cs
+++ api/Startup.cs
@@ -37,12 +37,10 @@ namespace api
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}"
-                );
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc();
         }
     }
 }
