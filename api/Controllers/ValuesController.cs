b6fb2b9faad669f02824162e4d6d5f044443f38d
Виталий Вострокнутов <iwnow@airmac-ivvnow.local>
2017-03-27 16:58:04 +0300

    +

--- api/Controllers/ValuesController.cs
+++ api/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "MVC" };
         }
 
         // GET api/values/5
